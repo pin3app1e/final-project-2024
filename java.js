ffc66e68bd7637a8f116205db5b9725caa611731
pin3app1e <brorsanemil@gmail.com>
2024-06-07 13:00:10 +0200

    updated fetch

--- java.js
+++ java.js
@@ -3,21 +3,25 @@
 function handleSubmit() {
     var url = document.getElementById("urlInput");
     var HTMLcode =  fetchWebpage(url);
-    console.log("HTMLkod: "+HTMLcode);
+    console.log("Url ===" + url);
+    console.log("HTMLkod" + HTMLcode);
 }
 
 function fetchWebpage(url) {
     fetch(url)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.text(); // or response.text() for plain text
-    })
-    .then(data => {
-        console.log('Success:', data);
+    .then(handleResponse)
+    .then(html => {
+        return(html);
     })
     .catch(error => {
         console.error('Error:', error);
     });
 }
+
+function handleResponse(response) {
+    if (!response.ok) {
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.text(); // Returnerar HTML som text
+}
+

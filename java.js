d98efa1277e9cf98f0a47ed8454c7790bc6f4269
pin3app1e <brorsanemil@gmail.com>
2024-06-08 13:54:27 +0200

    Main instead of frame

--- java.js
+++ java.js
@@ -28,9 +28,8 @@ function handleResponse(response) {
 
 function updateMainContent(html) {
     // Get the main element
-    //var mainElement = document.querySelector('body');
+    var mainElement = document.querySelector('main');
 
     // Set the inner HTML of the main element to the fetched HTML
-    //mainElement.innerHTML = html;
-    mainFrame.document.body.innerHtml = html;
+    mainElement.innerHTML = html;
 }
